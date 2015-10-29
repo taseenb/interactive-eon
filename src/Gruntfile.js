b6fc4c1cbdfa3b7a22e01076e4eb2f70ce6a80f5
taseenb <esteban.almiron@gmail.com>
2015-10-29 12:12:55 +0000

    update

--- src/Gruntfile.js
+++ src/Gruntfile.js
@@ -54,7 +54,7 @@ module.exports = function ( grunt ) {
     sass: {
       options: {
         sourceMap: true,
-        //outputStyle: 'compressed'
+        outputStyle: 'compressed'
       },
       dist: {
         files: {
